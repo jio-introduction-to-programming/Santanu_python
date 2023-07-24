6ebf3082f4304774214898ea28906137fcf7b159
Santanu-Sen <santanu.s24pgai@jioinstitute.edu.in>
2023-07-24 19:12:41 +0530

    Added assignments

---
+++ README.md
@@ -0,0 +1,5 @@
+# assignments
+This is the place to find the GitHub Assignments for "Introduction to Programming"
+
+# Session-1 
+Assignment: Complete the Code blocks! 
---
+++ session-3/assignment.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+total = 0
+for num in numbers:
+    if num == 3:
+        break
+    else:
+        total += num
+print(total)
+
+
+print("Complete all the quiz questions and push code to Git") 
